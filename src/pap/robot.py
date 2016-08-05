14fc6270be8cc801c6a12a121a882dcac4f31eb5
Jorge Cañardo Alastuey <jorgecanardo@gmail.com>
2016-08-05 16:01:16 -0600

    ENH: add jaco arm. Yay for inheritance!

--- src/pap/robot.py
+++ src/pap/robot.py
@@ -5,6 +5,8 @@ import numpy as np
 
 import rospy
 
+import actionlib
+
 from std_msgs.msg import Header
 from geometry_msgs.msg import Point, Quaternion, Pose, PoseStamped
 
@@ -13,6 +15,8 @@ import baxter_interface
 # from moveit_commander import conversions
 from baxter_core_msgs.srv import SolvePositionIK, SolvePositionIKRequest
 from baxter_core_msgs.msg import DigitalIOState
+
+import kinova_msgs.msg
 
 # Apparently tf was deprecated a long time ago? And we should use tf2?
 import tf
@@ -80,28 +84,11 @@ def limb_pose(limb_name):
       0.9983032  -0.00843487  0.0532936  -0.02189443]"""
 
 
-class Baxter(object):
-    """Add higher level operations to Baxter."""
-    def __init__(self, limb_name, gripper=None):
-        self.limb_name = limb_name
-        self.other_limb_name = {'right': 'left',
-                                'left': 'right'}[limb_name]
-
-        self.limb = baxter_interface.Limb(limb_name)
-        self.limb.set_joint_position_speed(0.1)
-
-        if gripper is None:
-            self.gripper = baxter_interface.Gripper(limb_name)
-        else:
-            self.gripper = gripper(limb_name)
-
-        # Enable actuators (doesn't seem to work?) I need to rosrun
-        # robot_enable.py -e
-        baxter_interface.RobotEnable().enable()
-        # Calibrate gripper
-        self.gripper.calibrate()
-
-        self.kinematics = baxter_kinematics(self.limb_name)
+class Robot(object):
+    def __init__(self, base):
+        """base : str
+            Robot base/root tf"""
+        self.base = base
         self.tl = tf.TransformListener()
 
     def pick(self, pose, direction=(0, 0, 1), distance=0.1):
@@ -114,11 +101,12 @@ class Baxter(object):
         self.move_ik(pregrasp_pose)
         rospy.sleep(0.5)
         # We want to block end effector opening so that the next
-        # movement happens with the gripper fully opened.
-        self.gripper.open(block=True)
+        # movement happens with the gripper fully opened. In Baxter,
+        # that involves sublcassing the gripper class
+        self.gripper.open()
         self.move_ik(pose)
         rospy.sleep(0.5)
-        self.gripper.close(block=True)
+        self.gripper.close()
         rospy.sleep(0.5)
         self.move_ik(pregrasp_pose)
 
@@ -132,9 +120,55 @@ class Baxter(object):
         rospy.sleep(0.5)
         self.move_ik(pose)
         rospy.sleep(0.5)
-        self.gripper.open(block=True)
+        self.gripper.open()
         rospy.sleep(0.5)
         self.move_ik(preplace_pose)
+
+    def translate(self, pose, direction, distance):
+        """Get an PoseStamped msg and apply a translation direction * distance
+
+        """
+        # Let's get the pose, move it to the tf frame, and then
+        # translate it
+        base_pose = self.tl.transformPose(self.base, pose)
+        base_pose.pose.position = Point(*np.array(direction) * distance +
+                                        Point2list(base_pose.pose.position))
+        return base_pose
+
+
+class BaxterGripper(baxter_interface.Gripper):
+    """Make open/close not take any args, to play nicely with our
+    superclass Robot"""
+    def open(self):
+        super(BaxterGripper, self).open(block=True)
+
+    def close(self):
+        super(BaxterGripper, self).close(block=True)
+
+
+class Baxter(Robot):
+    """Add higher level operations to Baxter."""
+    def __init__(self, limb_name, gripper=None):
+        super(Baxter, self).__init__(base='base')
+        self.limb_name = limb_name
+        self.other_limb_name = {'right': 'left',
+                                'left': 'right'}[limb_name]
+
+        self.limb = baxter_interface.Limb(limb_name)
+        self.limb.set_joint_position_speed(0.1)
+
+        if gripper is None:
+            self.gripper = BaxterGripper(limb_name)
+        else:
+            self.gripper = gripper(limb_name)
+
+        # Enable actuators (doesn't seem to work?) I need to rosrun
+        # robot_enable.py -e
+        baxter_interface.RobotEnable().enable()
+        # Calibrate gripper
+        self.gripper.calibrate()
+
+        self.kinematics = baxter_kinematics(self.limb_name)
 
     def compute_joint_velocities(self, cartesian_velocities, jinv=None):
         """Given cartesian end effector velocities (in the base coordinate
@@ -161,18 +195,6 @@ class Baxter(object):
         joint_v_dict.update(
             {'head_nod': 0.0, 'head_pan': 0.0, 'torso_t0': 0.0})
         return joint_v_dict
-
-    def translate(self, pose, direction, distance, tf='base'):
-        """Get an PoseStamped msg and apply a translation direction * distance
-        (in the frame tf).
-
-        """
-        # Let's get the pose, move it to the tf frame, and then
-        # translate it
-        base_pose = self.tl.transformPose("base", pose)
-        base_pose.pose.position = Point(*np.array(direction) * distance +
-                                        Point2list(base_pose.pose.position))
-        return base_pose
 
     def ik_quaternion(self, stamped_pose):
         """Take a PoseStamped and convert it to joint angles using IK. You can
@@ -258,6 +280,80 @@ class Baxter(object):
             return pose
         else:
             raise TypeError('Pose needs to be xyzrpy or xyzqxqyqzq')
+
+
+class JacoGripper(object):
+    def __init__(self, robot_type='j2n6a300'):
+        self.robot_type = robot_type
+        action_address = ('/{}_driver/fingers_action/'
+                          'finger_positions'.format(robot_type))
+        self.client = actionlib.SimpleActionClient(
+            action_address,
+            kinova_msgs.msg.SetFingersPositionAction)
+        self.client.wait_for_server()
+
+        self.finger_maxDist = 18.9/2/1000  # max distance for one finger
+        self.finger_maxTurn = 6800  # max thread rotation for one finger
+
+    def set_position(self, position):
+        """Accept position from 0 (open) to 100 (closed)."""
+        position_turns = np.clip(position, 0, 100) / 100 * self.finger_maxTurn
+        goal = kinova_msgs.msg.SetFingersPositionGoal()
+        goal.fingers.finger1 = float(position_turns[0])
+        goal.fingers.finger2 = float(position_turns[1])
+        # The MICO arm has only two fingers, but the same action
+        # definition is used
+        if len(position) < 3:
+            goal.fingers.finger3 = 0.0
+        else:
+            goal.fingers.finger3 = float(position_turns[2])
+        self.client.send_goal(goal)
+        if self.client.wait_for_result(rospy.Duration(5.0)):
+            return self.client.get_result()
+        else:
+            self.client.cancel_all_goals()
+            rospy.logwarn('        the gripper action timed-out')
+
+    def open(self):
+        self.set_position([0, 0, 0])
+
+    def close(self):
+        self.set_position([100, 100, 100])
+
+
+class Jaco(Robot):
+    def __init__(self, robot_type='j2n6a300', *args, **kwargs):
+        super(Jaco, self).__init__(base='root')
+        self.robot_type = robot_type
+        self.gripper = JacoGripper()
+
+    def move_ik(self, stamped_pose):
+        """Take a PoseStamped and move the arm there.
+
+        """
+        if not isinstance(stamped_pose, PoseStamped):
+            raise TypeError("No duck typing here? :(")
+        pose = stamped_pose.pose
+        position, orientation = pose.position, pose.orientation
+        # Send a cartesian goal to the action server. Adapted from kinova_demo
+        action_address = ('/' + self.robot_type +
+                          '_driver/pose_action/tool_pose')
+        client = actionlib.SimpleActionClient(action_address,
+                                              kinova_msgs.msg.ArmPoseAction)
+        client.wait_for_server()
+
+        goal = kinova_msgs.msg.ArmPoseGoal()
+        goal.pose.header = Header(frame_id=(self.robot_type + '_link_base'))
+        goal.pose.pose.position = position
+        goal.pose.pose.orientation = orientation
+
+        client.send_goal(goal)
+
+        if client.wait_for_result(rospy.Duration(10.0)):
+            return client.get_result()
+        else:
+            client.cancel_all_goals()
+            rospy.loginfo('        the cartesian action timed-out')
 
 
 def main(limb_name, reset):
